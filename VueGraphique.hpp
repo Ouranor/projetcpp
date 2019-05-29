d4003d91041bd2cf2bb492858cc795bb1b120ed7
qianbiPENG <51122254+qianbiPENG@users.noreply.github.com>
2019-05-29 23:44:54 +0200

    Add files via upload

--- VueGraphique.hpp
+++ VueGraphique.hpp
@@ -17,7 +17,7 @@ using namespace std;
 #include "drawArea.hpp"
 
 
-class VueG : public Gtk::Window, public Observateur<double> 
+class VueG : public Gtk::Window
 {
 	
 private:
@@ -33,8 +33,47 @@ private:
 	
 	
 public:
-	VueG();
-	virtual ~VueG();
+	VueG() :
+		box1(Gtk::ORIENTATION_HORIZONTAL),
+		box2(Gtk::ORIENTATION_VERTICAL),
+		begin("ok")
+		
+	{
+	set_size_request(1000, 1000);
+	set_title("Convertisseur");
+	// l’entrée
+	entry.set_max_length(50);
+	entry.set_text("Enter");
+	entry.select_region(0, entry.get_text_length());
+	
+	textBuffer1=Gtk::TextBuffer::create();
+	
+	ifstream in("aidefile.txt");
+	if(in.is_open()){
+		while(!in.eof()){
+		getline(in,line);
+		cout<<line<<endl;
+		textBuffer1->insert_at_cursor(line);
+		textBuffer1->insert_at_cursor("\n");		
+		}
+		in.close();
+	}
+	aideText.set_buffer(textBuffer1);
+	
+	aideText.move_child(aideText,50,50);
+	box1.pack_start(aideText);
+	box1.pack_start(entry);
+	box1.pack_start(begin);
+	
+	box2.add(box1);
+	drawArea.set_size_request(200,200);
+	box2.pack_start(drawArea,Gtk::PACK_SHRINK);
+	
+	
+	add(box2);
+	show_all_children();
+   }
+
 
 };
 
